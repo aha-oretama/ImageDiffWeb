6070780c7a76e6716e33b5da6ccb00423c53061e
yasufumi sekine <yasufumi_sekine@r.recruit.co.jp>
2016-11-07 02:13:14 +0900

    Initial Commit

---
+++ src/main/java/aha/oretama/jp/ImageDiffWebApplication.java
@@ -0,0 +1,12 @@
+package aha.oretama.jp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageDiffWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageDiffWebApplication.class, args);
+	}
+}
